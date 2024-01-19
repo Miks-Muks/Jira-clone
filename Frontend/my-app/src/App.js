1630c4e69200423c68a02bf28388b709f4dd74bd
NovikovDN <101504167+NovikovDN@users.noreply.github.com>
2024-01-19 19:02:28 +0700

    Added react

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,23 +1,11 @@
-import Header from "./comonents/header"
+import Header from "./comonents/header/header"
+import Center from "./comonents/center"
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header />
+        <Center />
+      </div>
   );
 }
 
